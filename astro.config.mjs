9ba91be8c3d795cd91f365841dc83977fc65ecf4
ErikXue <downnn@live.com>
2026-05-20 13:37:27 +0800

    更新了下index.md，修复了菜单栏展示

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,15 +16,27 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: '移动端手册',
+					label: '移动端最佳实践手册',
 					items: [
-						{ label: '手册总览', slug: 'guides/mobile' },
-						{ label: '设计目标', slug: 'guides/design_target' },
-						{ label: '1. 目标与场景', slug: 'guides/mobile/goals-scenarios' },
-						{ label: '2. 信息架构', slug: 'guides/mobile/information-architecture' },
-						{ label: '3. 交互与行为', slug: 'guides/mobile/interaction-behavior' },
-						{ label: '4. 视觉与感知', slug: 'guides/mobile/visual-perception' },
-						{ label: '5. 性能', slug: 'guides/mobile/performance' },
+						{
+							label: '设计思路和方向',
+							items: [
+								{ label: '手册总览', slug: 'guides/mobile' },
+								{ label: '1. 目标与场景', slug: 'guides/mobile/goals-scenarios' },
+								{ label: '2. 信息架构', slug: 'guides/mobile/information-architecture' },
+								{ label: '3. 交互与行为', slug: 'guides/mobile/interaction-behavior' },
+								{ label: '4. 视觉与感知', slug: 'guides/mobile/visual-perception' },
+								{ label: '5. 性能', slug: 'guides/mobile/performance' },
+							],
+						},
+						{
+							label: '实战训练和演示',
+							items: [{ label: '总览', slug: 'guides/practice' }],
+						},
+						{
+							label: '典型场景和功能',
+							items: [{ label: '总览', slug: 'guides/scenarios' }],
+						},
 					],
 				},
 			],
