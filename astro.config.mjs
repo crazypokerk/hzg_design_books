648e45667e2915ea1319bae07874d1d3dfe5d3e7
ErikXue <downnn@live.com>
2026-06-03 10:59:06 +0800

    NFC第一版内容完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,7 @@ export default defineConfig({
 								{ label: '调试', slug: 'guides/scenarios/how-to-debug-hac' },
 								{ label: '日志', slug: 'guides/scenarios/logs-hac' },
 								{ label: '「功能」 定位', slug: 'guides/scenarios/gps-location-demo' },
+								{ label: '「功能」 NFC', slug: 'guides/scenarios/nfc-tag-reading' },
 								{ label: '「功能」 离线模式', slug: 'guides/scenarios/offline-model-hac' },
 							],
 						},
--- src/content/docs/guides/scenarios/index.md
+++ src/content/docs/guides/scenarios/index.md
@@ -15,4 +15,5 @@ sidebar:
 - [调试](./how-to-debug-hac/)：使用 Chrome DevTools 调试 HAC 中的活字格 Web 页面。
 - [日志](./logs-hac/)：查看和收集 HAC 运行日志。
 - [「功能」定位](./gps-location-demo/)：通过 PDA 交互命令插件获取地理位置。
+- [「功能」NFC](./nfc-tag-reading/)：通过支持 NFC 的 Android 设备读取近场标签。
 - [「功能」离线模式](./offline-model-hac/)：通过离线模式和离线存储支持本地暂存。
